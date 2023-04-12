b5093c46339be631ad2212e504da346e07938759
pakkiadossraji <pakkiadossraji@gmail.com>
2023-04-12 15:27:58 +0500

    reply

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-
-    function myFunction(where) {
-      var x = document.getElementById("reply-box").value;
-      var newDiv = document.createElement('div');
-      var newpara = document.createElement('p');
-      newpara.classList.add('reply1');
-     
-      var newbtn = document.createElement('button');
-      var text = document.createTextNode('Delete');
-      newbtn.appendChild(text);
-      newbtn.classList.add('btn');
-      newbtn.onclick = function(){
-        newpara.remove();
-        if (newpara.style.display === "none") {
-          newbtn.style.display = "none";
-        } else {
-          newbtn.style.display = "none";
-        }
-      }
-     
-      var insert = document.getElementById('getId');
-      newpara.innerHTML = x;
+var select = document.querySelectorAll('.reply-btn');
+select.forEach((node,index) =>{
+  node.addEventListener('click', ()=>{
+        var x = document.querySelectorAll(".reply-box")[index].value;
+        console.log(x)
+        var newpara = document.createElement('p');
+        newpara.classList.add('reply1');
         
-        newDiv.appendChild(newpara);
-        where.parentNode.insertBefore(newDiv,insert);  
-        where.parentNode.insertBefore(newpara,newDiv); 
-        document.getElementById("reply-box").value = '';
-        where.parentNode.insertBefore(newbtn,newDiv)
+        var newbtn = document.createElement('button');
+        var text = document.createTextNode('Delete');
+          newbtn.appendChild(text);
+          newbtn.classList.add('btn');
+          newbtn.onclick = function(){
+            newpara.remove();
+            if (newpara.style.display === "none") {
+              newbtn.style.display = "none";
+            } else {
+              newbtn.style.display = "none";
+            }
+          }
+        var insert = node.closest(".parent").querySelector(".getpara")
+          newpara.innerHTML = x;
+        // where.parentNode.insertBefore(newDiv,insert);  
+          insert.appendChild(newpara);
+        // where.parentNode.insertBefore(newpara,newDiv); 
+          document.querySelectorAll(".reply-box")[index].value = '';
+          insert.appendChild(newbtn);
+        // where.parentNode.insertBefore(newbtn,newDiv)
         
-      }
+      })
+    })
